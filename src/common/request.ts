dcd85668ce79781af702d993fda1e8fe83568a04
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-15 20:55:25 +0900

    [refactor] 기본 메소드  분리

--- src/common/request.ts
+++ src/common/request.ts
@@ -4,29 +4,31 @@ import LocalStorage from "./storage";
 
 const HOST_URL = "http://localhost:80";
 
+const sendGetRequest = async (targetUrl: string, accessToken: any) => {
+  return await fetch(targetUrl, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: "include",
+  }).then((resp) => resp.json());
+};
+
+const sendPostRequest = async (targetUrl: string, accessToken: any, body: any) => {
+  return await fetch(targetUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(body),
+    credentials: "include",
+  }).then((resp) => resp.json());
+};
+
 const HostRequest = {
-  sendGetRequest: async function (targetUrl: string, accessToken: string) {
-    return await fetch(targetUrl, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      credentials: "include",
-    }).then((resp) => resp.json());
-  },
-  sendPostRequest: async function (targetUrl: string, accessToken: string, body: any) {
-    return await fetch(targetUrl, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify(body),
-      credentials: "include",
-    }).then((resp) => resp.json());
-  },
   sendAccessCode: async function (accessCode: string) {
     const requestURL = `${HOST_URL}/member/oauth/${accessCode}`;
     return await fetch(requestURL, {
@@ -40,24 +42,24 @@ const HostRequest = {
   sendDatabaseID: async function (notionDatabaseUrl: string) {
     const requestURL = `${HOST_URL}/member/notion-database-id`;
     const accessToken = await LocalStorage.get(StorageKey.ACCESS_TOKEN);
-    return await this.sendPostRequest(requestURL, accessToken, {
+    return await sendPostRequest(requestURL, accessToken, {
       notionDatabaseUrl,
     });
   },
   getMemberNotionInfo: async function () {
     const requestURL = `${HOST_URL}/member/notion-space`;
     const accessToken = await LocalStorage.get(StorageKey.ACCESS_TOKEN);
-    return await this.sendGetRequest(requestURL, accessToken);
+    return await sendGetRequest(requestURL, accessToken);
   },
   getAllProblemList: async function () {
     const requestURL = `${HOST_URL}/problem`;
     const accessToken = await LocalStorage.get(StorageKey.ACCESS_TOKEN);
-    return await this.sendGetRequest(requestURL, accessToken);
+    return await sendGetRequest(requestURL, accessToken);
   },
   saveNewProblem: async function (problem: Problem) {
     const requestURL = `${HOST_URL}/prolbem`;
     const accessToken = await LocalStorage.get(StorageKey.ACCESS_TOKEN);
-    return await this.sendPostRequest(requestURL, accessToken, {
+    return await sendPostRequest(requestURL, accessToken, {
       problem: {
         siteType: problem.site,
         number: problem.number,
@@ -69,7 +71,7 @@ const HostRequest = {
   isProblemExists: async function (problem: Problem) {
     const requestURL = `${HOST_URL}/prolbem/check`;
     const accessToken = await LocalStorage.get(StorageKey.ACCESS_TOKEN);
-    return await this.sendPostRequest(requestURL, accessToken, {
+    return await sendPostRequest(requestURL, accessToken, {
       problem: {
         siteType: problem.site,
         number: problem.number,
