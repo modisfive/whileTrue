31c5efe34822c50661072348f0b57fd4c146baf0
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-24 03:43:22 +0900

    [fix] 데이터베이스 저장 시에 메시지 출력

--- src/pages/databasePage.tsx
+++ src/pages/databasePage.tsx
@@ -7,28 +7,43 @@ import Utils from "../common/utils";
 
 const App: React.FC<{}> = () => {
   const [databaseUrl, setDatabaseUrl] = useState("");
-  const [isError, setIsError] = useState(false);
-  const [databaseStatus, setDatabaseStatus] = useState(true);
+  const [isValidUrl, setIsValidUrl] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isSavedSucceed, setIsSavedSucceed] = useState(true);
 
   const handleChange = (e) => {
     const url = e.target.value;
     setDatabaseUrl(url);
-    setIsError(!Utils.validateNotionDatabaseUrl(url));
+    setIsValidUrl(Utils.validateNotionDatabaseUrl(url));
   };
 
   const handleSubmit = () => {
     if (!Utils.validateNotionDatabaseUrl(databaseUrl)) {
       return;
     }
-
     chrome.runtime.sendMessage(
-      {
-        from: "options",
-        subject: "databaseUrl",
-        databaseUrl: databaseUrl,
-      },
-      (resp) => setDatabaseStatus(resp)
+      { from: "options", subject: "databaseUrl", databaseUrl: databaseUrl },
+      (resp) => {
+        setIsSubmitted(true);
+        setIsSavedSucceed(resp);
+      }
     );
+  };
+
+  const msg = () => {
+    if (isSubmitted) {
+      if (isSavedSucceed) {
+        return <span className="desc">Notion 데이터베이스가 저장되었습니다.</span>;
+      } else {
+        return <span className="desc desc-error">형식을 확인해주세요</span>;
+      }
+    } else {
+      if (databaseUrl === "" || isValidUrl) {
+        return <span className="desc">Notion 데이터베이스 URL을 공유해주세요.</span>;
+      } else {
+        return <span className="desc desc-error">Notion Database URL 형식에 맞지 않습니다.</span>;
+      }
+    }
   };
 
   return (
@@ -57,14 +72,7 @@ const App: React.FC<{}> = () => {
                   Submit
                 </Button>
               </div>
-
-              {isError ? (
-                <span className="desc desc-error">Notion Database URL 형식에 맞지 않습니다.</span>
-              ) : databaseStatus ? (
-                <span className="desc">Notion Database URL을 입력해주세요.</span>
-              ) : (
-                <span className="desc desc-error">형식을 확인해주세요</span>
-              )}
+              {msg()}
             </Form>
           </Col>
         </Row>
