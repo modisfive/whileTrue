3f0c6d36be45333d7c78da03e39217d6c8812b1e
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-28 07:19:08 +0900

    [fix] SiteType 수정

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,9 +1,8 @@
 enum SiteType {
-  BOJ = "백준",
-  PROGRAMMERS = "프로그래머스",
-  SWEA = "SWEA",
-  LEETCODE = "LeetCode",
-  DEFAULT = "DEFAULT_SITE",
+  BOJ,
+  PROGRAMMERS,
+  SWEA,
+  LEETCODE,
 }
 
 enum IconType {
