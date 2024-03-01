c89e6e7633337be1f48bc6fe918825a548f3bc74
Lee, Nam Gon <nglee.dev@gmail.com>
2024-03-01 22:15:21 +0900

    [chore] LocalStorage에서 Promise 삭제

--- src/common/storage.ts
+++ src/common/storage.ts
@@ -2,14 +2,10 @@ import { StorageKey } from "./constants";
 
 const LocalStorage = {
   get: async function (key: StorageKey) {
-    return await new Promise((resolve) => {
-      chrome.storage.local.get([key]).then((result) => resolve(result[key]));
-    });
+    return await chrome.storage.local.get([key]).then((result) => result[key]);
   },
   set: async function (key: StorageKey, value: any) {
-    await new Promise((resolve) => {
-      chrome.storage.local.set({ [key]: value }).then(resolve);
-    });
+    await chrome.storage.local.set({ [key]: value });
   },
   remove: async function (key: StorageKey) {
     await chrome.storage.local.remove(key);
