9b54ec7676aefc6efd51edd2239d1fd8868add74
Lee, Nam Gon <nglee.dev@gmail.com>
2024-03-01 17:59:14 +0900

    [env] build 시 console 출력 삭제

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,6 +12,9 @@ module.exports = merge(common, {
       new TerserPlugin({
         extractComments: false,
         terserOptions: {
+          compress: {
+            drop_console: true,
+          },
           format: {
             comments: false,
           },
