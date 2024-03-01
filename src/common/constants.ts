6ca527de4f3c0bcfc526ecf4e9482b5eb9bb4028
Lee, Nam Gon <nglee.dev@gmail.com>
2024-03-01 20:26:09 +0900

    [fix] redirect url 수정

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -23,7 +23,7 @@ enum StorageKey {
 }
 
 enum OAuth {
-  NOTION_AUTH_URL = "https://api.notion.com/v1/oauth/authorize?client_id=36993609-798b-4704-ab63-e8f864eecfdd&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fwhiletrue%2Fmember%2Foauth%2Fredirect",
+  NOTION_AUTH_URL = "https://api.notion.com/v1/oauth/authorize?client_id=36993609-798b-4704-ab63-e8f864eecfdd&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fwhiletrue.co.kr%2Fmember%2Foauth%2Fredirect",
 }
 
 export { SiteType, IconType, SiteHost, StorageKey, OAuth };
