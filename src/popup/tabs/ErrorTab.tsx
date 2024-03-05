64a68c7ceff6a9ea9c22456001776c36ee14f800
Lee, Nam Gon <nglee.dev@gmail.com>
2024-03-05 14:31:53 +0900

    [chore] 에러 메시지 명시적으로 수정

--- src/popup/tabs/ErrorTab.tsx
+++ src/popup/tabs/ErrorTab.tsx
@@ -9,7 +9,7 @@ const ErrorTab: React.FC<{}> = () => {
     <Container className="h-75">
       <div className="h-100 d-flex flex-column justify-content-center">
         <FontAwesomeIcon icon={faTriangleExclamation} size="2xl" style={{ color: "#FFD43B" }} />
-        <span>노션 데이터베이스의 칼럼이 적절하지 않습니다.</span>
+        <span>노션 데이터베이스의 칼럼 형식이 적절하지 않습니다.</span>
       </div>
     </Container>
   );
