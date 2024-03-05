aba46d2b561d495247b4f9266c01545319a6be86
Lee, Nam Gon <nglee.dev@gmail.com>
2024-03-05 14:25:32 +0900

    [fix] navbar mb 삭제

--- src/pages/databasePage.tsx
+++ src/pages/databasePage.tsx
@@ -82,7 +82,7 @@ const App: React.FC<{}> = () => {
 
   return (
     <Container className="App">
-      <Navbar className="shadow p-3 mb-5 bg-body-tertiary rounded" style={{ height: "7%" }}>
+      <Navbar className="shadow p-3 bg-body-tertiary rounded" style={{ height: "7%" }}>
         <Navbar.Brand className="d-flex align-items-center">
           <Image style={{ width: "auto", height: 40 }} src={"/icon.png"} className="me-1" />
           whileTrue 노션 데이터베이스 설정
