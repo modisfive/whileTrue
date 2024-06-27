ffa1c51247d4b8e17afdfca3e160930a0d2b0ccf
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-27 16:09:15 +0900

    [refactor] 사용하지 않는 enum 삭제

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -23,11 +23,6 @@ enum StorageKey {
   IS_ERROR = "is_error",
 }
 
-enum OAuth {
-  REDIRECT_HOST = "whiletrue.co.kr",
-  NOTION_AUTH_URL = "https://api.notion.com/v1/oauth/authorize?client_id=36993609-798b-4704-ab63-e8f864eecfdd&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fwhiletrue.co.kr%2Fmember%2Foauth%2Fredirect",
-}
-
 enum RESP_STATUS {
   SUCCESS = "success",
   FAILED = "failed",
@@ -35,4 +30,4 @@ enum RESP_STATUS {
   INVALID = "invalid",
 }
 
-export { SiteType, IconType, SiteHost, StorageKey, OAuth, RESP_STATUS };
+export { SiteType, IconType, SiteHost, StorageKey, RESP_STATUS };
