72e9c2b56ff273f75b957e58e8b39087abfd5b80
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-27 17:19:26 +0900

    [refactor] 컴포넌트 분리

--- src/popup/tabs/RandomSelectTab.tsx
+++ src/popup/tabs/RandomSelectTab.tsx
@@ -36,40 +36,48 @@ const RandomSelectTab: FC<{ setIsError: CallableFunction }> = ({ setIsError }) =
     });
   };
 
+  const body = () => {
+    if (isOnProgress) {
+      return (
+        <div className="h-100 d-flex justify-content-center align-items-center">
+          <Spinner animation="border" />
+        </div>
+      );
+    }
+    if (problemPage === undefined) {
+      return (
+        <div className="h-100 d-flex justify-content-center align-items-center">
+          <span>문제를 선택해주세요.</span>
+        </div>
+      );
+    }
+    return (
+      <>
+        <div>
+          <Row className="justify-content-center">
+            <Image
+              style={{ width: "auto", height: 70 }}
+              src={Utils.selectLogo(problemPage.siteType)}
+            />
+          </Row>
+          <Row>
+            <span>
+              {problemPage.number}. {problemPage.title}
+            </span>
+          </Row>
+        </div>
+        <Row className="mt-4">
+          <Button variant="primary" onClick={handleClick1}>
+            바로가기
+          </Button>
+        </Row>
+      </>
+    );
+  };
+
   return (
     <Container className="h-100 d-flex flex-column justify-content-evenly">
-      <div style={{ height: "50%" }}>
-        {isOnProgress ? (
-          <div className="h-100 d-flex justify-content-center align-items-center">
-            <Spinner animation="border" />
-          </div>
-        ) : problemPage === undefined ? (
-          <div className="h-100 d-flex justify-content-center align-items-center">
-            <span>문제를 선택해주세요.</span>
-          </div>
-        ) : (
-          <>
-            <div>
-              <Row className="justify-content-center">
-                <Image
-                  style={{ width: "auto", height: 70 }}
-                  src={Utils.selectLogo(problemPage.siteType)}
-                />
-              </Row>
-              <Row>
-                <span>
-                  {problemPage.number}. {problemPage.title}
-                </span>
-              </Row>
-            </div>
-            <Row className="mt-4">
-              <Button variant="primary" onClick={handleClick1}>
-                바로가기
-              </Button>
-            </Row>
-          </>
-        )}
-      </div>
+      <div style={{ height: "50%" }}>{body()}</div>
       <Row>
         <Button variant="secondary" onClick={handleClick2}>
           문제 선택하기
