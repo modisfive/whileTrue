6aee29b023b817e19ccfc4427c1623a0f41f911b
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-06 17:47:09 +0900

    [refactor] ProblemInsertTab 리팩토링

--- src/popup/tabs/ProblemInsertTab.tsx
+++ src/popup/tabs/ProblemInsertTab.tsx
@@ -6,26 +6,14 @@ import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import Utils from "../../common/utils";
 import { RESP_STATUS } from "../../common/constants";
 
-const ProblemInsertTab: FC<{ problemPage: ProblemPage; setIsError: CallableFunction }> = ({
-  problemPage,
-  setIsError,
-}) => {
-  const [saveResult, setSaveResult] = useState(false);
-  const [isOnProgress, setIsOnProgress] = useState(true);
+interface Props {
+  problemPage: ProblemPage;
+  setIsError: CallableFunction;
+}
 
-  const handleSubmit = () => {
-    setIsOnProgress(true);
-    chrome.runtime.sendMessage({ from: "popup", subject: "insertProblem", problemPage }, (resp) => {
-      setIsOnProgress(false);
-      if (resp === RESP_STATUS.FAILED) {
-        setIsError(true);
-        setSaveResult(false);
-      } else {
-        setIsError(false);
-        setSaveResult(true);
-      }
-    });
-  };
+const ProblemInsertTab: FC<Props> = ({ problemPage, setIsError }) => {
+  const [saveResult, setSaveResult] = useState<boolean | null>(null);
+  const [isOnProgress, setIsOnProgress] = useState(true);
 
   useEffect(() => {
     chrome.runtime.sendMessage(
@@ -42,38 +30,46 @@ const ProblemInsertTab: FC<{ problemPage: ProblemPage; setIsError: CallableFunct
     );
   }, []);
 
+  const handleSubmit = () => {
+    setIsOnProgress(true);
+    chrome.runtime.sendMessage({ from: "popup", subject: "insertProblem", problemPage }, (resp) => {
+      setIsOnProgress(false);
+      if (resp === RESP_STATUS.FAILED) {
+        setIsError(true);
+        setSaveResult(false);
+      } else {
+        setIsError(false);
+        setSaveResult(true);
+      }
+    });
+  };
+
+  const renderSaveButton = () => {
+    if (isOnProgress) return <Spinner animation="border" />;
+    if (saveResult) return <FontAwesomeIcon icon={faCircleCheck} size="2x" color="green" />;
+    return (
+      <Button variant="success" onClick={handleSubmit}>
+        저장하기
+      </Button>
+    );
+  };
+
   return (
     <Container className="h-100 d-flex flex-column justify-content-evenly">
       <div style={{ height: "50%" }}>
-        <div>
-          <Row className="justify-content-center">
-            <Image
-              style={{ width: "auto", height: 70 }}
-              src={Utils.selectLogo(problemPage.siteType)}
-            />
-          </Row>
-          <Row>
-            <span>
-              {problemPage.number}. {problemPage.title}
-            </span>
-          </Row>
-        </div>
+        <Row className="justify-content-center">
+          <Image
+            style={{ width: "auto", height: 70 }}
+            src={Utils.selectLogo(problemPage.siteType)}
+          />
+        </Row>
+        <Row>
+          <span>
+            {problemPage.number}. {problemPage.title}
+          </span>
+        </Row>
       </div>
-      <Row>
-        {isOnProgress ? (
-          <div className="d-flex justify-content-center align-items-center">
-            <Spinner animation="border" />
-          </div>
-        ) : saveResult ? (
-          <div className="d-flex justify-content-center align-items-center">
-            <FontAwesomeIcon icon={faCircleCheck} size="2xl" color="green" />
-          </div>
-        ) : (
-          <Button variant="success" onClick={handleSubmit}>
-            저장하기
-          </Button>
-        )}
-      </Row>
+      <Row className="d-flex justify-content-center align-items-center">{renderSaveButton()}</Row>
     </Container>
   );
 };
