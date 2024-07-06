c53a5fde7d97571f9772358baa0c08b9109f3929
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-06 23:00:11 +0900

    [refactor] optionsHandler 리팩토링

--- src/background/optionsHandler.ts
+++ src/background/optionsHandler.ts
@@ -4,39 +4,50 @@ import LocalStorage from "../common/storage";
 import CheckDatabaseResponseDto from "../api/dto/response/CheckDatabaseResponseDto";
 import Utils from "../common/utils";
 
-const handleMessageFromOptions = (request: any, sendResponse: any) => {
+const handleDatabaseUrl = async (request: any, sendResponse: CallableFunction) => {
+  const databaseId = Utils.parseNotionDatabaseId(request.databaseUrl);
+  const resp: CheckDatabaseResponseDto = await HostRequest.validateUserNotion(
+    request.notionApiKey,
+    databaseId
+  );
+
+  if (resp.validCheck === RESP_STATUS.SUCCESS) {
+    await LocalStorage.set(StorageKey.NOTION_API_KEY, request.notionApiKey);
+    await LocalStorage.set(StorageKey.DATABASE_ID, resp.databaseId);
+  }
+  await LocalStorage.set(StorageKey.RESP_STATUS, resp.validCheck);
+  sendResponse(resp.validCheck);
+};
+
+const handleDatabasePage = async () => {
+  const databasePage = `chrome-extension://${chrome.runtime.id}/database.html`;
+  await chrome.tabs.create({ url: databasePage, selected: true });
+};
+
+const handleExit = async () => {
+  await Promise.all([
+    LocalStorage.remove(StorageKey.NOTION_API_KEY),
+    LocalStorage.remove(StorageKey.DATABASE_ID),
+    LocalStorage.remove(StorageKey.PROBLEM_PAGE_LIST),
+    LocalStorage.remove(StorageKey.RESP_STATUS),
+  ]);
+
+  const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+  await chrome.tabs.remove(tabs[0].id);
+};
+
+const handleMessageFromOptions = (request: any, sendResponse: CallableFunction) => {
   switch (request.subject) {
     case "databaseUrl":
-      const databaseId = Utils.parseNotionDatabaseId(request.databaseUrl);
-      HostRequest.validateUserNotion(request.notionApiKey, databaseId).then(
-        (resp: CheckDatabaseResponseDto) => {
-          if (resp.validCheck == RESP_STATUS.SUCCESS) {
-            LocalStorage.set(StorageKey.NOTION_API_KEY, request.notionApiKey);
-            LocalStorage.set(StorageKey.DATABASE_ID, resp.databaseId);
-          }
-          LocalStorage.set(StorageKey.RESP_STATUS, resp.validCheck).then(() =>
-            sendResponse(resp.validCheck)
-          );
-        }
-      );
+      handleDatabaseUrl(request, sendResponse);
       break;
 
     case "databasePage":
-      const databasePage = `chrome-extension://${chrome.runtime.id}/database.html`;
-      chrome.tabs.create({ url: databasePage, selected: true }).then(() => sendResponse());
+      handleDatabasePage();
       break;
 
     case "exit":
-      Promise.all([
-        LocalStorage.remove(StorageKey.NOTION_API_KEY),
-        LocalStorage.remove(StorageKey.DATABASE_ID),
-        LocalStorage.remove(StorageKey.PROBLEM_PAGE_LIST),
-        LocalStorage.remove(StorageKey.RESP_STATUS),
-      ]).then(() => {
-        chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-          chrome.tabs.remove(tabs[0].id);
-        });
-      });
+      handleExit();
       break;
 
     default:
