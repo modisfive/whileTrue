f99b7b7ed97b707e386e8330c7fd422cfb143824
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-06 23:23:45 +0900

    [refactor] 컴포넌넌트 분리

--- src/pages/databasePage.tsx
+++ src/pages/databasePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { FC, useState, ChangeEvent } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import { Button, Col, Container, Form, Image, Navbar, Row, Spinner } from "react-bootstrap";
@@ -6,23 +6,42 @@ import "./databasePage.css";
 import Utils from "../common/utils";
 import { RESP_STATUS } from "../common/constants";
 
-const renderProgressMessage = () => <span className="desc">저장 중...</span>;
-const renderSuccessMessage = () => (
-  <span className="desc desc-success">Notion 데이터베이스가 저장되었습니다.</span>
-);
-const renderInvalidMessage = () => (
-  <span className="desc desc-error">데이터베이스 칼럼명과 속성이 올바르지 않습니다.</span>
-);
-const renderNotFoundMessage = () => (
-  <span className="desc desc-error">
-    공유한 워크스페이스와 페이지 아래에 있거나, 데이터베이스 형식인지 확인해주세요.
-  </span>
-);
-const renderUrlInvalidMessage = () => (
-  <span className="desc desc-error">노션 데이터베이스 URL 형식에 맞지 않습니다.</span>
+const App: FC = () => {
+  return (
+    <Container className="App">
+      <CustomNavbar />
+      <Container
+        style={{ width: "90%", height: "93%" }}
+        className="d-flex flex-column justify-content-center"
+      >
+        <GuideRow />
+        <FormRow />
+        <LinkRow />
+      </Container>
+    </Container>
+  );
+};
+
+const CustomNavbar: FC = () => (
+  <Navbar className="shadow p-3 bg-body-tertiary rounded" style={{ height: "7%" }}>
+    <Navbar.Brand className="d-flex align-items-center">
+      <Image style={{ width: "auto", height: 40 }} src={"/icon.png"} className="me-1" />
+      whileTrue 노션 데이터베이스 설정
+    </Navbar.Brand>
+  </Navbar>
 );
 
-const App: React.FC = () => {
+const GuideRow: FC = () => (
+  <Row className="mb-5">
+    <Col className="d-flex justify-content-center">
+      <a href="https://school.programmers.co.kr/learn/challenges?order=recent">
+        <Button variant="link">Notion 연결 매뉴얼 바로가기</Button>
+      </a>
+    </Col>
+  </Row>
+);
+
+const FormRow: FC = () => {
   const [notionApiKey, setNotionApiKey] = useState<string>("");
   const [databaseUrl, setDatabaseUrl] = useState<string>("");
   const [isValidUrl, setIsValidUrl] = useState<boolean>(false);
@@ -86,65 +105,64 @@ const App: React.FC = () => {
   };
 
   return (
-    <Container className="App">
-      <Navbar className="shadow p-3 bg-body-tertiary rounded" style={{ height: "7%" }}>
-        <Navbar.Brand className="d-flex align-items-center">
-          <Image style={{ width: "auto", height: 40 }} src={"/icon.png"} className="me-1" />
-          whileTrue 노션 데이터베이스 설정
-        </Navbar.Brand>
-      </Navbar>
-      <Container
-        style={{ width: "90%", height: "93%" }}
-        className="d-flex flex-column justify-content-center"
-      >
-        <Row className="mb-5">
-          <Col className="d-flex justify-content-center">
-            <a href="https://school.programmers.co.kr/learn/challenges?order=recent">
-              <Button variant="link">Notion 연결 매뉴얼 바로가기</Button>
-            </a>
-          </Col>
-        </Row>
-        <Row className="mb-5">
-          <Form className="flex align-items-center justify-content-center">
-            <Form.Group className="mb-5">
-              <Form.Label>Notion API Key를 입력해주세요.</Form.Label>
-              <Form.Control
-                placeholder="Notion API Key"
-                value={notionApiKey}
-                onChange={handleApiKeyChange}
-                type="password"
-              />
-            </Form.Group>
-            <Form.Group className="mb-5">
-              <Form.Label>문제를 저장할 Notion 데이터베이스 링크를 공유해주세요.</Form.Label>
-              <Form.Control
-                placeholder="Notion 데이터베이스 링크"
-                value={databaseUrl}
-                onChange={handleDatabaseUrlChange}
-              />
-              <Form.Text className="text-muted">{renderMessage()}</Form.Text>
-            </Form.Group>
-            <Button onClick={handleSubmit} className="w-100">
-              {isOnProgress ? <Spinner animation="border" size="sm" /> : "공유하기"}
-            </Button>
-          </Form>
-        </Row>
-        <Row className="d-flex">
-          <Col className="d-flex justify-content-center">
-            <a href="https://www.acmicpc.net/">
-              <Button variant="link">백준 바로가기</Button>
-            </a>
-          </Col>
-          <Col className="d-flex justify-content-center">
-            <a href="https://school.programmers.co.kr/learn/challenges?order=recent">
-              <Button variant="link">프로그래머스 바로가기</Button>
-            </a>
-          </Col>
-        </Row>
-      </Container>
-    </Container>
+    <Row className="mb-5">
+      <Form className="flex align-items-center justify-content-center">
+        <Form.Group className="mb-5">
+          <Form.Label>Notion API Key를 입력해주세요.</Form.Label>
+          <Form.Control
+            placeholder="Notion API Key"
+            value={notionApiKey}
+            onChange={handleApiKeyChange}
+            type="password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-5">
+          <Form.Label>문제를 저장할 Notion 데이터베이스 링크를 공유해주세요.</Form.Label>
+          <Form.Control
+            placeholder="Notion 데이터베이스 링크"
+            value={databaseUrl}
+            onChange={handleDatabaseUrlChange}
+          />
+          <Form.Text className="text-muted">{renderMessage()}</Form.Text>
+        </Form.Group>
+        <Button onClick={handleSubmit} className="w-100">
+          {isOnProgress ? <Spinner animation="border" size="sm" /> : "공유하기"}
+        </Button>
+      </Form>
+    </Row>
   );
 };
+
+const LinkRow: FC = () => (
+  <Row className="d-flex">
+    <Col className="d-flex justify-content-center">
+      <a href="https://www.acmicpc.net/">
+        <Button variant="link">백준 바로가기</Button>
+      </a>
+    </Col>
+    <Col className="d-flex justify-content-center">
+      <a href="https://school.programmers.co.kr/learn/challenges?order=recent">
+        <Button variant="link">프로그래머스 바로가기</Button>
+      </a>
+    </Col>
+  </Row>
+);
+
+const renderProgressMessage = () => <span className="desc">저장 중...</span>;
+const renderSuccessMessage = () => (
+  <span className="desc desc-success">Notion 데이터베이스가 저장되었습니다.</span>
+);
+const renderInvalidMessage = () => (
+  <span className="desc desc-error">데이터베이스 칼럼명과 속성이 올바르지 않습니다.</span>
+);
+const renderNotFoundMessage = () => (
+  <span className="desc desc-error">
+    공유한 워크스페이스와 페이지 아래에 있거나, 데이터베이스 형식인지 확인해주세요.
+  </span>
+);
+const renderUrlInvalidMessage = () => (
+  <span className="desc desc-error">노션 데이터베이스 URL 형식에 맞지 않습니다.</span>
+);
 
 const container = document.createElement("div");
 container.setAttribute("id", "root");
