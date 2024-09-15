5bad2e715be9d4f109e81d0743a8ad1750741958
Lee, Nam Gon <nglee.dev@gmail.com>
2024-09-15 20:22:12 +0900

    [refactor] 티어 값들 분리

--- src/contentScript/baekjoon.ts
+++ src/contentScript/baekjoon.ts
@@ -2,6 +2,40 @@ import { IconType } from "../common/enums/icon.enum";
 import { SiteType } from "../common/enums/site.enum";
 import { ProblemPage } from "../common/models/problem-page.model";
 
+const LEVEL_TIERS = [
+  "Unrated",
+  "Bronze 5",
+  "Bronze 4",
+  "Bronze 3",
+  "Bronze 2",
+  "Bronze 1",
+  "Silver 5",
+  "Silver 4",
+  "Silver 3",
+  "Silver 2",
+  "Silver 1",
+  "Gold 5",
+  "Gold 4",
+  "Gold 3",
+  "Gold 2",
+  "Gold 1",
+  "Platinum 5",
+  "Platinum 4",
+  "Platinum 3",
+  "Platinum 2",
+  "Platinum 1",
+  "Diamond 5",
+  "Diamond 4",
+  "Diamond 3",
+  "Diamond 2",
+  "Diamond 1",
+  "Ruby 5",
+  "Ruby 4",
+  "Ruby 3",
+  "Ruby 2",
+  "Ruby 1",
+];
+
 const getSolvedAcInfo = async (problemNumber: string) => {
   return await new Promise((resolve) => {
     chrome.runtime.sendMessage(
@@ -11,44 +45,8 @@ const getSolvedAcInfo = async (problemNumber: string) => {
   });
 };
 
-const convertLevelTier = (level: number) => {
-  const levels = [
-    "Unrated",
-    "Bronze 5",
-    "Bronze 4",
-    "Bronze 3",
-    "Bronze 2",
-    "Bronze 1",
-    "Silver 5",
-    "Silver 4",
-    "Silver 3",
-    "Silver 2",
-    "Silver 1",
-    "Gold 5",
-    "Gold 4",
-    "Gold 3",
-    "Gold 2",
-    "Gold 1",
-    "Platinum 5",
-    "Platinum 4",
-    "Platinum 3",
-    "Platinum 2",
-    "Platinum 1",
-    "Diamond 5",
-    "Diamond 4",
-    "Diamond 3",
-    "Diamond 2",
-    "Diamond 1",
-    "Ruby 5",
-    "Ruby 4",
-    "Ruby 3",
-    "Ruby 2",
-    "Ruby 1",
-  ];
-  if (0 <= level && level < levels.length) {
-    return levels[level];
-  }
-  return "Unrated";
+const convertLevelTier = (level: number): string => {
+  return LEVEL_TIERS[level] || "Unrated";
 };
 
 const parseProblemNumber = () => {
